6833486cc5caa68a850a2489b01d87bcb752354c
Andrew Liu <andrewliu@Andrews-MacBook-Pro-2.local>
2020-02-13 21:56:37 -0800

    exercise not useful and waste of time.

---
+++ Week6/exercise18-1/exercise.R
@@ -0,0 +1,54 @@
+# load relevant libraries
+library("httr")
+library("jsonlite")
+
+# Be sure and check the README.md for complete instructions!
+
+
+# Use `source()` to load your API key variable from the `apikey.R` file you made.
+# Make sure you've set your working directory!
+source(paste(getwd(), "apikey.R", sep = "/"))
+
+# Create a variable `movie_name` that is the name of a movie of your choice.
+movie_name <- "VFW"
+
+# Construct an HTTP request to search for reviews for the given movie.
+# The base URI is `https://api.nytimes.com/svc/movies/v2/`
+# The resource is `reviews/search.json`
+# See the interactive console for parameter details:
+#   https://developer.nytimes.com/movie_reviews_v2.json
+#
+# You should use YOUR api key (as the `api-key` parameter)
+# and your `movie_name` variable as the search query!
+base_url <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+query_param <- list("api-key" = api_key, "movie_name" = movie_name)
+
+
+# Send the HTTP Request to download the data
+# Extract the content and convert it from JSON
+res <- GET(paste(base_url, resource, sep = ""), query = query_param)
+body <- content(res, "text")
+data <- fromJSON(body)
+
+# What kind of data structure did this produce? A data frame? A list?
+typeof(data) # a list
+
+# Manually inspect the returned data and identify the content of interest 
+# (which are the movie reviews).
+# Use functions such as `names()`, `str()`, etc.
+names(data)
+
+# Flatten the movie reviews content into a data structure called `reviews`
+reviews <- data$results
+
+# From the most recent review, store the headline, short summary, and link to
+# the full article, each in their own variables
+headline <- reviews %>% filter(display_title == movie_name) %>% pull(headline)
+summary <- reviews %>% filter(display_title == movie_name) %>% pull(summary_short)
+link <- reviews %>% filter(display_title == movie_name) %>% pull(link) %>% pull(url)
+
+# Create a list of the three pieces of information from above. 
+# Print out the list.
+info <- list(headline = headline, summary = summary, link = link)
+print(info)
